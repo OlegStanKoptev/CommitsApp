c86963d15bcb1e7458fea006e537ca2128daa924
Oleg Koptev <olegstankotpev@gmail.com>
2020-11-29 22:37:22 +0300

    Code is finished

---
+++ Commit+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Commit+CoreDataClass.swift
+//  CommitsApp
+//
+//  Created by Oleg Koptev on 29.11.2020.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Commit)
+public class Commit: NSManagedObject {
+
+}
